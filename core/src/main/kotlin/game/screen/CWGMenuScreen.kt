17275db5f364c722e4a31215ab1eea0069b23e08
Dave LeCompte <tsmaster@gmail.com>
2024-07-14 15:04:43 -0700

    Added Clickable Buttons on Menu screen

--- core/src/main/kotlin/game/screen/CWGMenuScreen.kt
+++ core/src/main/kotlin/game/screen/CWGMenuScreen.kt
@@ -6,6 +6,8 @@ import com.badlogic.gdx.controllers.Controller
 import com.badlogic.gdx.controllers.Controllers
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.Sprite
+import com.badlogic.gdx.math.Vector2
+import com.badlogic.gdx.utils.Array
 import com.badlogic.gdx.utils.viewport.FitViewport
 import game.CWGGame
 import ktx.graphics.use
@@ -34,6 +36,8 @@ class CWGMenuScreen(game: CWGGame) : CWGScreen(game) {
         setSize(2.0f, 0.5f)
     }
 
+    private var buttonBBoxes = Array<Pair<Vector2, Vector2>>()
+
     override fun show() {
         LOG.debug{ "showing CWGMenuScreen" }
 
@@ -42,10 +46,20 @@ class CWGMenuScreen(game: CWGGame) : CWGScreen(game) {
         btn1sprite.setPosition(8.0f, 8.0f)
         btn2sprite.setPosition(8.0f, 7.0f)
         btn3sprite.setPosition(8.0f, 6.0f)
+        buttonBBoxes.run {
+            add(Pair(Vector2(8.0f, 8.0f), Vector2(2.0f, 0.5f)))
+            add(Pair(Vector2(8.0f, 7.0f), Vector2(2.0f, 0.5f)))
+            add(Pair(Vector2(8.0f, 6.0f), Vector2(2.0f, 0.5f)))
+        }
 
         LOG.info { "enumerating detected game controllers" }
         for (controller: Controller in Controllers.getControllers()) {
             LOG.info{ " found controller: " + controller.name }
+        }
+
+        val curController:Controller = Controllers.getCurrent()
+        if (curController != null) {
+            LOG.info { "current controller is: " + curController.name }
         }
     }
 
@@ -67,15 +81,54 @@ class CWGMenuScreen(game: CWGGame) : CWGScreen(game) {
         }
 
         if (Gdx.input.isKeyJustPressed(Input.Keys.NUM_1)) {
-            game.setScreen<CWG2dGameScreenAshley>()
+            dispatchNumeric(0)
         }
         if (Gdx.input.isKeyJustPressed(Input.Keys.NUM_2)) {
-            game.setScreen<CWG2dGameScreenFleks>()
+            dispatchNumeric(1)
         }
         if (Gdx.input.isKeyJustPressed(Input.Keys.NUM_3)) {
             // exit game
-            game.setExit()
+            dispatchNumeric(2)
         }
+
+        if (Gdx.input.isButtonJustPressed(Input.Buttons.LEFT)) {
+            val clickPos: Vector2 = Vector2(Gdx.input.x.toFloat(), Gdx.input.y.toFloat())
+            val clickPosUnp = viewport.unproject(clickPos)
+            LOG.info {
+                "click at " + clickPos + clickPosUnp
+            }
+
+            val buttonIndex = findButton(clickPosUnp)
+            LOG.info {
+                "button index " + buttonIndex
+            }
+
+            dispatchNumeric(buttonIndex)
+        }
+    }
+
+    private fun dispatchNumeric(buttonIndex: Int) {
+        when (buttonIndex) {
+            0 -> game.setScreen<CWG2dGameScreenAshley>()
+            1 -> game.setScreen<CWG2dGameScreenFleks>()
+            2 -> game.setExit()
+            else -> {}
+        }
+    }
+
+    private fun findButton(clickPosUnp: Vector2): Int {
+        for (i: Int in 0 ..< buttonBBoxes.size) {
+            val buttonBox = buttonBBoxes[i]
+            val lleft = buttonBox.first
+            val sz = buttonBox.second
+            if ((clickPosUnp.x >= lleft.x) &&
+                (clickPosUnp.x <= lleft.x + sz.x) &&
+                (clickPosUnp.y >= lleft.y) &&
+                (clickPosUnp.y <= lleft.y + sz.y)) {
+                return i
+            }
+        }
+        return -1
     }
 
     override fun dispose() {
