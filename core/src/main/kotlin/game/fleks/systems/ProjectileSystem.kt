bae1491ac300c695ae6e156283d6db1c8145b3c0
Dave LeCompte <tsmaster@gmail.com>
2024-07-15 08:20:13 -0700

    WIP: Fleks update - updated ProjectileSystem

--- core/src/main/kotlin/game/fleks/systems/ProjectileSystem.kt
+++ core/src/main/kotlin/game/fleks/systems/ProjectileSystem.kt
@@ -3,60 +3,53 @@ package game.fleks.systems
 import com.github.quillraven.fleks.Entity
 import com.github.quillraven.fleks.IteratingSystem
 import com.github.quillraven.fleks.World.Companion.family
+import game.fleks.components.DamageableComponent
 import game.fleks.components.ProjectileComponent
 import game.fleks.components.FLTransformComponent
+import game.fleks.components.RemoveComponent
 
-/*
-// TODO: update to Fleks
-
-private val LOG = logger<ProjectileSystem>()
-
-class ProjectileSystem : IteratingSystem(
-    allOf(ProjectileComponent::class,
-          TransformComponent::class).get()) {
-
-    override fun processEntity(entity: Entity, deltaTime: Float) {
-        val transform = entity[TransformComponent.mapper]
-        require(transform != null) { "Transform not set" }
-        val projectile = entity[ProjectileComponent.mapper]
-        require(projectile != null) { "Projectile not set" }
-
-        //LOG.debug {"processing projectile"}
+class ProjectileSystem() : IteratingSystem (
+    family = family { all(
+        ProjectileComponent,
+        FLTransformComponent
+    )}
+) {
+    override fun onTickEntity(entity: Entity) {
+        val transform = entity[FLTransformComponent]
+        val projectile = entity[ProjectileComponent]
 
         projectile.lifetimeLeftSeconds -= deltaTime
         if (projectile.lifetimeLeftSeconds <= 0.0f)
         {
-            //LOG.debug{"timing out projectile"}
-            entity.addComponent<RemoveComponent>(engine)
+            entity.configure {
+                it += RemoveComponent()
+            }
             return
         }
-        //LOG.debug{"moving projectile ${projectile.velocity}"}
-        transform.interpolatedPosition.x += projectile.velocity.x * deltaTime
-        transform.interpolatedPosition.y += projectile.velocity.y * deltaTime
+
+        transform.position.x += projectile.velocity.x * deltaTime
+        transform.position.y += projectile.velocity.y * deltaTime
 
         // check collision with damageable
-        val damageableEntities = engine.getEntitiesFor(
-            allOf(
-                DamageableComponent::class,
-                TransformComponent::class).get()
-        )
+        val damageableEntities = world.family {
+            all(DamageableComponent,
+                FLTransformComponent)
+        }
 
-        for (dmgEntity in damageableEntities) {
-            val dmgTransform = dmgEntity[TransformComponent.mapper]
-            require(dmgTransform != null) { "Transform not set" }
-            val dmgDamage = dmgEntity[DamageableComponent.mapper]
-            require(dmgDamage != null) { "Transform not set" }
+        damageableEntities.forEach { dmgEntity ->
+            val dmgTransform = dmgEntity[FLTransformComponent]
+            val dmgDamage = dmgEntity[DamageableComponent]
 
             if (dmgDamage.dmgTeamIndex == projectile.teamIndex)
             {
-                continue
+                return@forEach
             }
 
-            val dmg_x = dmgTransform.interpolatedPosition.x
-            val dmg_y = dmgTransform.interpolatedPosition.y
+            val dmg_x = dmgTransform.position.x
+            val dmg_y = dmgTransform.position.y
 
-            val delta_x = transform.interpolatedPosition.x - dmg_x
-            val delta_y = transform.interpolatedPosition.y - dmg_y
+            val delta_x = transform.position.x - dmg_x
+            val delta_y = transform.position.y - dmg_y
 
             val COLLIDE_DIST = 1.0f
             val COLLIDE_DIST_SQR = COLLIDE_DIST * COLLIDE_DIST
@@ -66,27 +59,18 @@ class ProjectileSystem : IteratingSystem(
                 dmgDamage.curHealth -= projectile.damagePayload
 
                 // remove projectile
-                entity.addComponent<RemoveComponent>(engine)
+                entity.configure {
+                    it += RemoveComponent()
+                }
 
                 if (dmgDamage.curHealth <= 0.0f)
                 {
                     // TODO make explosion
-                    dmgEntity.addComponent<RemoveComponent>(engine)
+                    dmgEntity.configure {
+                        it += RemoveComponent()
+                    }
                 }
             }
         }
     }
-}
-*/
-
-class ProjectileSystem() : IteratingSystem (
-    family = family { all(
-        ProjectileComponent,
-        FLTransformComponent
-    )}
-) {
-    override fun onTickEntity(entity: Entity) {
-        TODO("Not yet implemented")
-    }
-
 }
